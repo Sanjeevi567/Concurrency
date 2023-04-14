ecd57bd4d0cbfa5ce1f2050a8293e762948693ee
Sanjeevi <sanjeevimani567@gmail.com>
2023-04-14 06:24:26 -0700

    Create Dereference logic bug for copy types.rs

---
+++ Dereference logic bug for copy types.rs
@@ -0,0 +1,22 @@
+use std::sync::{Arc,Mutex};
+fn main(){
+    let m=Arc::new(Mutex::new([1,2,3,4,5]));
+    let clone =m.clone();
+    let m1 = std::thread::spawn(move||{
+     
+     //Trying to move the data to the caller.
+     //For move types this is an error.
+     //But for copy types this is problem and then we have a logic bug
+      *clone.lock().unwrap()
+        
+    //solution.
+    //The paranthese are important ,otherwise confusing compile error shows up.
+   //(*clone.lock().unwrap())[0]=12;
+        
+        
+}).join().unwrap();
+println!("{:?}",m.lock().unwrap());
+println!("{:?}",m1);
+}
+
+ 
