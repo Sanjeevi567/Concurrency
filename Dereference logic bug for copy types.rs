e4618d4858c487fe1101cbf357ccf8ea7efea441
Sanjeevi <sanjeevimani567@gmail.com>
2023-04-14 22:46:06 -0700

    Update Dereference logic bug for copy types.rs

--- Dereference logic bug for copy types.rs
+++ Dereference logic bug for copy types.rs
@@ -7,7 +7,7 @@ fn main(){
      //Trying to move the data to the caller.
      //For move types this is an error.
      //But for copy types this is problem and then we have a logic bug
-      let task = *clone.lock().unwrap();
+       *clone.lock().unwrap()
         
     //solution.
     //The paranthese are important ,otherwise confusing compile error shows up.
@@ -16,7 +16,6 @@ fn main(){
         
 }).join().unwrap();
 println!("{:?}",m.lock().unwrap());
+//This is independent of the mutex inner type
 println!("{:?}",m1);
 }
-
- 
